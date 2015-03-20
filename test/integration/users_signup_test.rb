d3e59d036c8744631782d266316afc46a5b10c90
Mario Goncalves <mario.goncalves@wiremaze.com>
2015-03-20 09:18:19 +0000

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,6 +24,7 @@ test "invalid signup information" do
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert is_logged_in?
   end
   
 end
